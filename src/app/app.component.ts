e0f51d2ac0c1533236d81b8cfb2b702bfe3ebfd2
김석곤 <krdn.net@gmail.com>
2018-11-30 11:17:38 +0900

    shared 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'aio-shell',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
